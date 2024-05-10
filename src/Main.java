502bcfc4488c048d1afb799c60501797cd2fee90
Artem Vasyatkin <dustler98@gmail.com>
2024-05-10 16:20:24 +0300

    Created the Person service class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person sarah = new Person("Sarah", 30);
+        if (sarah.isAdult()) {
+            System.out.println("Иди в бар");
+        } else {
+            System.out.println("Иди в школу");
+        }
+        System.out.println(sarah);
+        sarah.increaseAge(-30);
+
     }
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,35 @@
 public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public boolean isAdult() {
+        return getAge() > 18;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void increaseAge(int increment) {
+        if (increment < 0) {
+            throw new IllegalArgumentException("Инкримент не может быть отрицательным");
+        }
+        this.age = age + increment;
+    }
+
+    public String toString() {
+        return "Имя " + this.name + " Возраст " + this.age;
+    }
 }
